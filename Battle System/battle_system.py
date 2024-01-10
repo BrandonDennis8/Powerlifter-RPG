a251d051cc58d2efb8f7dff6eac8d33667e95a0b
Brandon Dennis <brandondennis325@gmail.com>
2024-01-09 20:04:36 -0600

    Update battle_system.py

--- Battle System/battle_system.py
+++ Battle System/battle_system.py
@@ -1,35 +1,64 @@
+PLAYER = 1
+ENEMY = 2
+
+
 USE_ITEM = 2
 REG_ATTACK = 3
 SPECIAL_ATTACK = 5
 OVERDRIVE = 7
 
+
+
 class playable:
-    def __init__(self, n, s):
-        self.name = n
-        self.speed = s
+    def __init__(self, Player, Name, Speed):
+        self.player = Player
+        self.name = Name
+        self.speed = Speed
+
     counter = 0
     
 def getSpeed(obj):
     return obj.speed
     
+def startBattle(participants):
+    # Sort priority queue initially by speed
+    participants.sort(key= getSpeed, reverse=True)
+    print("\n*** battle start ***")
+    printNames(participants)
     
-def StartBattle(participants):
-    participants.sort(key= getSpeed)
-    print("Sorted:\n\n", participants)
+    # Check if counter == 0 for any players/enemies
+    for participant in participants:
+        if(participant.counter == 0):
+            # Check if enemy or player
+            if(participants[0].player == True):
+                # Make attack selection
+                pass
+            else:
+                # Enemy attack
+                pass
+
+    # Decrement all counters
+    for participant in participants:
+        participant.counter -= 1
+
+
+def printNames(participants):
+    for player in participants:
+        print(player.name)
     
-print("Whatsup")
     
-player1 = playable("Platz", 11)
-player2 = playable("Maddox", 8)
-player3 = playable("Magnusson", 10)
+player1 = playable(PLAYER, "Platz", 11)
+player2 = playable(PLAYER, "Maddox", 8)
+player3 = playable(PLAYER, "Magnusson", 10)
 
-enemy4 = playable("WADA Employee 1", 7)
-enemy5 = playable("WADA Employee 2", 7)
+enemy1 = playable(ENEMY, "WADA Employee 1 ", 7)
+enemy2 = playable(ENEMY, "WADA Employee 2" , 7)
 
-party = {player1,player2,player3,enemy1,enemy2}
+party = [player1,player2,player3,enemy1,enemy2]
 
-print(party)
-StartBattle(party)
+print("*** before battle ***")
+printNames(party)
+startBattle(party)
 
 
 
