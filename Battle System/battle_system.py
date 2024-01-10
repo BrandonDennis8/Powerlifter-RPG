3de0a9ad0564c02e48b2d4fef244f14462efd120
Brandon Dennis <brandondennis325@gmail.com>
2024-01-09 20:25:51 -0600

    Update battle_system.py

--- Battle System/battle_system.py
+++ Battle System/battle_system.py
@@ -20,26 +20,42 @@ class playable:
 def getSpeed(obj):
     return obj.speed
     
+def playerAttack(player):
+    print(f"{player.name}'s turn:\n  [1] Attack\n  [2] Item")
+    choice = input()
+    print(f"{player.name} Attacks!")
+    player.counter += 3
+
+def enemyAttack(enemy):
+    print(f"{enemy.name}'s turn")
+    print(f"{enemy.name} Attacks!")
+    enemy.counter += 3
+
+
 def startBattle(participants):
     # Sort priority queue initially by speed
+    battleOver = False
     participants.sort(key= getSpeed, reverse=True)
     print("\n*** battle start ***")
     printNames(participants)
     
-    # Check if counter == 0 for any players/enemies
-    for participant in participants:
-        if(participant.counter == 0):
-            # Check if enemy or player
-            if(participants[0].player == True):
-                # Make attack selection
-                pass
-            else:
-                # Enemy attack
-                pass
+    while(battleOver == False):
+        # Check if counter == 0 for any players/enemies
+        for participant in participants:
+            if(participant.counter == 0):
+                # Check if enemy or player
+                if(participant.player == PLAYER):
+                    print(f"\nCounter before attack: {participant.counter}")
+                    playerAttack(participant)
+                    print(f"Counter after attack: {participant.counter}\n")
+                elif(participant.player == ENEMY):
+                    print(f"\nCounter before attack: {participant.counter}")
+                    enemyAttack(participant)
+                    print(f"Counter after attack: {participant.counter}\n")
 
-    # Decrement all counters
-    for participant in participants:
-        participant.counter -= 1
+        # Decrement all counters
+        for participant in participants:
+            participant.counter -= 1
 
 
 def printNames(participants):
@@ -51,7 +67,7 @@ player1 = playable(PLAYER, "Platz", 11)
 player2 = playable(PLAYER, "Maddox", 8)
 player3 = playable(PLAYER, "Magnusson", 10)
 
-enemy1 = playable(ENEMY, "WADA Employee 1 ", 7)
+enemy1 = playable(ENEMY, "WADA Employee 1 ", 12)
 enemy2 = playable(ENEMY, "WADA Employee 2" , 7)
 
 party = [player1,player2,player3,enemy1,enemy2]
