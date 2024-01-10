1f8484b33cf66a6853634b92cbfeb870ae50e028
Brandon Dennis <brandondennis325@gmail.com>
2024-01-09 20:51:54 -0600

    Update battle_system.py

--- Battle System/battle_system.py
+++ Battle System/battle_system.py
@@ -1,3 +1,5 @@
+import time
+
 PLAYER = 1
 ENEMY = 2
 
@@ -10,10 +12,11 @@ OVERDRIVE = 7
 
 
 class playable:
-    def __init__(self, Player, Name, Speed):
+    def __init__(self, Player, Name, Speed, Overdrive):
         self.player = Player
         self.name = Name
         self.speed = Speed
+        self.overdrive = Overdrive
 
     counter = 0
     
@@ -21,7 +24,8 @@ def getSpeed(obj):
     return obj.speed
     
 def playerAttack(player):
-    print(f"{player.name}'s turn:\n  [1] Attack\n  [2] Item")
+    time.sleep(1)
+    print(f"\n{player.name}'s turn:\n  [1] Attack\n  [2] Item\n  [3] Overdrive")
     validInput = False
 
     while(validInput == False):
@@ -30,19 +34,22 @@ def playerAttack(player):
 
         match choice:
             case "1":
-                print(f"{player.name} attacks!")
+                print(f"{player.name} attacks!\n")
+                player.counter += REG_ATTACK
             case "2":
-                print(f"{player.name} used an item!")
+                print(f"{player.name} used an item!\n")
+                player.counter += USE_ITEM
+            case "3":
+                print(f"{player.name} used overdrive: {player.overdrive}!\n")
+                player.counter += OVERDRIVE
             case _:
                 print("incorrect action, try again")
                 validInput = False
 
-    
-    player.counter += REG_ATTACK
-
 def enemyAttack(enemy):
-    print(f"{enemy.name}'s turn")
-    print(f"{enemy.name} attacks!")
+    time.sleep(1)
+    print(f"\n{enemy.name}'s turn")
+    print(f"{enemy.name} attacks!\n")
     enemy.counter += REG_ATTACK
 
 
@@ -59,13 +66,13 @@ def startBattle(participants):
             if(participant.counter == 0):
                 # Check if enemy or player
                 if(participant.player == PLAYER):
-                    print(f"\nCounter before attack: {participant.counter}")
+                    # print(f"\nCounter before attack: {participant.counter}")
                     playerAttack(participant)
-                    print(f"Counter after attack: {participant.counter}\n")
+                    # print(f"Counter after attack: {participant.counter}\n")
                 elif(participant.player == ENEMY):
-                    print(f"\nCounter before attack: {participant.counter}")
+                    # print(f"\nCounter before attack: {participant.counter}")
                     enemyAttack(participant)
-                    print(f"Counter after attack: {participant.counter}\n")
+                    # print(f"Counter after attack: {participant.counter}\n")
 
         # Decrement all counters
         for participant in participants:
@@ -77,12 +84,12 @@ def printNames(participants):
         print(player.name)
     
     
-player1 = playable(PLAYER, "Platz", 11)
-player2 = playable(PLAYER, "Maddox", 8)
-player3 = playable(PLAYER, "Magnusson", 10)
+player1 = playable(PLAYER, "Platz", 11, "Quad Beam")
+player2 = playable(PLAYER, "Maddox", 8, "Pec Slam")
+player3 = playable(PLAYER, "Grigsby", 10, "Deadlift of Doom")
 
-enemy1 = playable(ENEMY, "WADA Employee 1 ", 12)
-enemy2 = playable(ENEMY, "WADA Employee 2" , 7)
+enemy1 = playable(ENEMY, "WADA Employee 1 ", 12, "n/a")
+enemy2 = playable(ENEMY, "WADA Employee 2" , 7, "n/a")
 
 party = [player1,player2,player3,enemy1,enemy2]
 
